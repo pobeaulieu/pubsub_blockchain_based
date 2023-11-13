0c656f89541481365bd7ae163d639918b7ca0967
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-11-13 16:03:46 -0500

    Restructure repository

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# Publish/Subscribe on the Blockchain
-## Getting Started
-### Install node mules 
-`npm installù
-### Deploy studentNumberContract using Truffle and Ganache
+
+# Getting Started (Prerequisites)
 1. Create a network on Ganache and start it
 ```
 hostname = 127.0.0.1
@@ -10,14 +7,21 @@ Port number = 7545
 Network ID = 5777
 ```
 
-2. Deploy the Contract
+2. Install node modules 
+`npm install`
+
+## Part 1 Student Number Contract 
+### Depoyment
 - update the `.env` file to provide the private key of the deployment sender account. This key can be obtained via the Ganache UI. 
 - <img src="readmeImages/ganachekey.png" alt="i1" style="width:800px;"/>
 - Run deployment (migration) with ` truffle migrate --network ganache --reset`
+### Tests
+- `truffle test --network ganache --reset`
 
-### Run Tests on Deployed Contract
-- Run command `truffle test --network ganache --reset`
+### Web3 Client
+- In `.env` file, update `CONTRACT_ADDRESS` with contract address received from deployment and `CLIENT_PRIVATE_KEY` (Can be found in Ganache UI the same way as documented in Deployment step)
+- Run Web3 client with `node studentNumberClient.js`. 
 
-
+# Part 2 Publish/Subscribe on the Blockchain
 ## System Architecture
 - <img src="readmeImages/architecture.png" alt="i1" style="width:800px;"/>
--- studentNumberClient.js
+++ studentNumberClient.js
@@ -1,32 +1,23 @@
 const {Web3} = require('web3');
 const fs = require('fs');
-
 require('dotenv').config(); // Load environment variables from .env file
 
-const contractAddress = process.env.CONTRACT_ADDRESS;
 const privateKey = process.env.CLIENT_PRIVATE_KEY;
 const localNodeUrl = process.env.NETWORK_URL;
-
-console.log(privateKey)
 const web3 = new Web3(localNodeUrl);
 
-// Replace 'gas' and 'gasPrice' with appropriate values
-const gas = 200000;
-const gasPrice = web3.utils.toWei('10', 'gwei');
-
-// Replace 'studentNumber' with the value you want to set
-const studentNumber = 123;
-
-// Create a contract object using the contract ABI and address
 const contractJson = JSON.parse(fs.readFileSync('build/contracts/StudentContract.json', 'utf8'));
 const contractAbi = contractJson.abi;
-
+const contractAddress = contractJson.networks[process.env.NETWORK_ID].address;
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
 
-// Specify the amount of ether to send with the transaction
+console.log(contractAddress)
+const gas = 200000;
+const gasPrice = web3.utils.toWei('10', 'gwei');
 const amountToSend = web3.utils.toWei('0.0054', 'ether');
 
 // Create a transaction to write the student number to the contract
+const studentNumber = 123;
 const setStudentNumberTransaction = contract.methods.setStudentNumber(studentNumber);
 const setStudentNumberEncodedTransaction = setStudentNumberTransaction.encodeABI();
 
